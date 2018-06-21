01b95548dd409af63031c7ad217f05683ba0d2c4
muzhiyun <my616422@126.com>
2018-06-22 00:40:22 +0800

    Add files via upload

---
+++ tomcatrestart.bat
@@ -0,0 +1,19 @@
+@echo off
+mode con cols=60 lines=10
+cd /d "%~dp0"
+cacls.exe "%SystemDrive%\System Volume Information" >nul 2>nul
+if %errorlevel%==0 goto Admin
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+echo Set RequestUAC = CreateObject^("Shell.Application"^)>"%temp%\getadmin.vbs"
+echo RequestUAC.ShellExecute "%~s0","","","runas",1 >>"%temp%\getadmin.vbs"
+echo WScript.Quit >>"%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs" /f
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+exit
+
+:Admin
+mode con cols=60 lines=10
+net stop tomcat9
+net start tomcat9
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+exit
